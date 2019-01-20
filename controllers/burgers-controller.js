4ba3b2b2d938e402cc412f102f0fb0f63d056736
adoration29 <Adora Okogeri>
2019-01-20 15:38:48 -0500

    burger assignment

---
+++ controllers/burgers-controller.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.all(function(burger_data){
+        console.log(burger_data);
+        res.render("index");
+    });
+     
+    });
+
+    router.put('/burgers/update',function(req,res){
+        burger.update(req.body.burger_id, function(result){
+console.log(result);
+res.redirect('/');
+        });
+    });
+
+    
+    module.exports= router;
+  
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers(burger_name,devoured) VALUES ('Potatoes Burger', FALSE);
+INSERT INTO burgers(burger_name,devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers(burger_name,devoured) VALUES ('Veggie Burger', FALSE);
+
